5a7f915d486b10ceb06537303e30d3e28e10381e
hakobhayrapetyan67-sys <hakob.hayrapetyan67@gmail.com>
2026-03-04 17:26:57 +0400

    Update task1_8.js

--- 260226/task1_8.js
+++ 260226/task1_8.js
@@ -1,13 +1,13 @@
-function foo(x, y){
-	for(let a = 0; a < x.length; ++a) {
-		for(let b = 0; b < y.length; ++b) {
-			if(x[a] == y[b]) {
+function foo(x, y) {
+	for (let a = 0; a < x.length; ++a) {
+		for (let b = 0; b < y.length; ++b) {
+			if (x[a] === y[b]) {
 				return true;
 			}
 		}
 	}
 	return false;
-}	
+}
 
 let x = prompt("enter the string");
 let y = prompt("enter the string");
