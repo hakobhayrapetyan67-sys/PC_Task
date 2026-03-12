972de4d0f85f2fc638fba3b0fb1141b55b68d4d1
Hakob <hakob.hayrapetyan67@gmail.com>
2026-03-12 19:18:00 +0400

    test

---
+++ 10032026t/dynamicProperty.js
@@ -0,0 +1,8 @@
+
+let key = "name";
+
+let obj = {
+	[key] : "Armen"
+};
+
+console.log(obj);
---
+++ 10032026t/forOf.js
@@ -0,0 +1,9 @@
+let obj = {
+	1 : "start",
+	2 : "end"
+};
+
+
+for(let i of Object.values(obj)){
+	console.log(i);		
+}
