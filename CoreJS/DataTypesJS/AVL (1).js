1d3246369f8e0852db17fc94986fc27bdb8622fa
hakobhayrapetyan67-sys <hakob.hayrapetyan67@gmail.com>
2026-08-11 11:39:15 +0400

    Update AVL (1).js

--- CoreJS/DataTypesJS/AVL (1).js
+++ CoreJS/DataTypesJS/AVL (1).js
@@ -3,7 +3,7 @@ class AVLNode {
         this.val = val;
         this.left = null;
         this.right = null;
-        this.height = 1; // Նոր Node-ի բարձրությունը սկզբում 1 է
+        this.height = 1; 
     }
 }
 
@@ -12,63 +12,58 @@ class AVLTree {
         this.root = null;
     }
 
-    // 1. Օգնող ֆունկցիա Node-ի բարձրությունը ստանալու համար
+   
     getHeight(node) {
         return node ? node.height : 0;
     }
 
-    // 2. Հաշվում է Balance Factor-ը (Left Height - Right Height)
+   
     getBalanceFactor(node) {
         return node ? this.getHeight(node.left) - this.getHeight(node.right) : 0;
     }
 
-    // 3. Թարմացնում է Node-ի բարձրությունը
+   
     updateHeight(node) {
         node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
     }
-
-    // ------------------ 🔄 ROTATIONS ------------------
-
-    //  Right Rotation (LL Case-ի համար)
     rotateRight(y) {
         let x = y.left;
         let T2 = x.right;
 
-        // Պտույտ
         x.right = y;
         y.left = T2;
 
-        // Բարձրությունների թարմացում (նախ y-ն, հետո x-ը)
+   
         this.updateHeight(y);
         this.updateHeight(x);
 
-        return x; // Նոր root-ը դառնում է x-ը
+        return x;
     }
 
-    // Left Rotation (RR Case-ի համար)
+  
     rotateLeft(x) {
         let y = x.right;
         let T2 = y.left;
 
-        // Պտույտ
+     
         y.left = x;
         x.right = T2;
 
-        // Բարձրությունների թարմացում
+      
         this.updateHeight(x);
         this.updateHeight(y);
 
-        return y; // Նոր root-ը դառնում է y-ը
+        return y; 
     }
 
-    // ------------------ ➕ INSERTION ------------------
+  
 
     insert(val) {
         this.root = this._insertNode(this.root, val);
     }
 
     _insertNode(node, val) {
-        // 1. Սովորական BST Insert
+
         if (!node) return new AVLNode(val);
 
         if (val < node.val) {
@@ -76,34 +71,33 @@ class AVLTree {
         } else if (val > node.val) {
             node.right = this._insertNode(node.right, val);
         } else {
-            return node; // Դուբլիկատ արժեքներ չենք թույլատրում
+            return node; 
         }
 
-        // 2. Թարմացնում ենք բարձրությունը
+
         this.updateHeight(node);
 
-        // 3. Ստուգում ենք Balance Factor-ը
+    
         let balance = this.getBalanceFactor(node);
 
-        // 4. Եթե ծառը անբալանս է դարձել, անում ենք համապատասխան պտույտները.
+      
 
-        // Left Left (LL) Case
+   
         if (balance > 1 && val < node.left.val) {
             return this.rotateRight(node);
         }
 
-        // Right Right (RR) Case
         if (balance < -1 && val > node.right.val) {
             return this.rotateLeft(node);
         }
 
-        // Left Right (LR) Case
+     
         if (balance > 1 && val > node.left.val) {
             node.left = this.rotateLeft(node.left);
             return this.rotateRight(node);
         }
 
-        // Right Left (RL) Case
+
         if (balance < -1 && val < node.right.val) {
             node.right = this.rotateRight(node.right);
             return this.rotateLeft(node);
@@ -112,7 +106,6 @@ class AVLTree {
         return node;
     }
 
-    // ------------------ 🗑️ DELETION ------------------
 
     delete(val) {
         this.root = this._deleteNode(this.root, val);
@@ -125,7 +118,6 @@ class AVLTree {
     }
 
     _deleteNode(node, val) {
-        // 1. Սովորական BST Delete
         if (!node) return null;
 
         if (val < node.val) {
@@ -133,9 +125,7 @@ class AVLTree {
         } else if (val > node.val) {
             node.right = this._deleteNode(node.right, val);
         } else {
-            // Node-ը գտնվել է
-
-            // Դեպք 1 ու 2: 1 կամ 0 երեխա
+       
             if (!node.left || !node.right) {
                 let temp = node.left ? node.left : node.right;
                 if (!temp) {
